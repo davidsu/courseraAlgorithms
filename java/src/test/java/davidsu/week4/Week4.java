6eef135bca91a2748b6d3e3af2b3e0ed6c7efebe
David <davidsu@wix.com>
2019-03-13 21:39:52 +0200

    prepare for week4

---
+++ java/src/test/java/davidsu/week4/Week4.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import java.util.*;
+
+public class Week4 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
